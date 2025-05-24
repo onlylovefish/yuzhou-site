6cccd3fb52a3909286511bdb54b9de899b0f759e
onlylovefish <2511452548@qq.com>
2025-05-24 23:49:43 +0800

    feat: 测试部署

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 import {zhNotes} from './notes'
 export default defineUserConfig({
-  base: '/',
+  base: '/yuzhou-site/',
   lang: 'zh-CN',
   locales: {
     '/': {
